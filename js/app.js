b4f66c78f834f75737f72f382835fe43870db717
FAXec <xecfahim.ahmed@gmail.com>
2021-12-28 17:19:28 +0000

    updated app.js

--- js/app.js
+++ js/app.js
@@ -1,24 +1,98 @@
 //Variables
 const lightboxEnabled = document.querySelectorAll('.lightbox-enabled');
+const lightboxArray = Array.from(lightboxEnabled);
+const lastImage = lightboxArray.length - 1;
 const lightboxContainer = document.querySelector('.lightbox-container');
 const lightboxImage = document.querySelector('.lightbox-image');
+
+const lightboxBtns = document.querySelectorAll('.lightbox-btn');
+const lightboxBtnRight = document.querySelector('#right');
+const lightboxBtnLeft = document.querySelector('#left');
+
+let activeImage = '';
+
 
 
 
 //functions
+const showLightBox = () => {lightboxContainer.classList.add('active')}
+const hideLightBox = () => {lightboxContainer.classList.remove('active')}
 
+const setActiveImage = (image) =>{
+    lightboxImage.src = image.dataset.imagesrc
+    activeImage = lightboxArray.indexOf(image)
+    // console.log(activeImage)
+
+switch (activeImage) {
+    case 0:        
+    lightboxBtnLeft.classList.add('inactive');
+    break;
+    case lastImage:
+    lightboxBtnRight.classList.add('inactive');
+    break;
+    default:
+    lightboxBtns.forEach(btn =>{
+        btn.classList.remove('inactive');
+    });
+    break;
+}}
+
+
+const removeBtnAnimation = () =>{
+  lightboxBtns.forEach(btn =>{
+    setTimeout(()  => {
+      btn.blur()},200);
+    })}
+
+
+
+const transitionSlidesLeft = () => {
+    lightboxBtnLeft.focus();
+    activeImage === 0 ? setActiveImage(lightboxArray[lastImage]): setActiveImage(lightboxArray[activeImage].previousElementSibling);
+    removeBtnAnimation();
+    
+}
+const transitionSlidesRight = () => {
+    lightboxBtnRight.focus();
+    activeImage === lastImage ? setActiveImage(lightboxArray[0]): setActiveImage(lightboxArray[activeImage].nextElementSibling);
+    removeBtnAnimation();
+}
+
+
+const transitionSlideHandler = (moveItems) =>{
+    moveItems.includes('left') ? transitionSlidesLeft() : transitionSlidesRight();
+
+}
 
 
 
 //event listeners
 lightboxEnabled.forEach(image=>{
     image.addEventListener('click', (e)=>{
-        console.log(e.target);
-
-
+        showLightBox();
+        setActiveImage(image);        
     });
-
-
 });
+
+lightboxContainer.addEventListener('click', ()=>{
+    // console.log('container clicked')
+    hideLightBox();
+});
+
+
+lightboxBtns.forEach(btn => {
+    btn.addEventListener('click', (e)=>{
+        e.stopPropagation();
+        transitionSlideHandler(e.currentTarget.id);
+        // console.log(e.currentTarget.id);
+    });    
+});
+
+
+
+
+
+
+
 
 
